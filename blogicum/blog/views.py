84ccd8a9e2ec341b27b3ab5c235b8e3723c8ceba
Александр Кузнецов <notjustespo@yandex.ru>
2023-09-18 17:22:21 +0300

    first final

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,17 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from datetime import datetime
+from django.db.models import Q
 
-from blog.models import Post
+from blog.models import Post, Category
 
 def index(request):
     template = 'blog/index.html'
-    post_list = Post.objects.all().filter(is_published=True).order_by('title')[:5]
+    post_list = Post.objects.all().filter(
+        is_published=True,
+        category__is_published=True,
+        pub_date__lte=datetime.now()
+        ).order_by('-pub_date')[:5]
     context = {'post_list': post_list}
     return render(request, template, context)
 
 def post_detail(request, pk):
     template = 'blog/detail.html'
-    post = Post.objects.get(pk=pk)
+    post = get_object_or_404(
+        Post.objects.all().filter(
+            pk=pk,
+            pub_date__lte=datetime.now(),
+            is_published=True,
+            category__is_published=True),
+    )
     context = {
         'post': post,
     }
@@ -19,9 +30,15 @@ def post_detail(request, pk):
 
 def category_posts(request, category_slug):
     template = 'blog/category.html'
+    category = get_object_or_404(Category.objects.all().filter(
+        slug=category_slug,
+        is_published=True))
     post_list = Post.objects.select_related('category').filter(
+        category=category,
         is_published=True,
         category__is_published=True,
+        pub_date__lte=datetime.now(),
     ).order_by('category')
-    context = {'post_list': post_list}
+    context = {'category': category,
+               'post_list': post_list}
     return render(request, template, context)
