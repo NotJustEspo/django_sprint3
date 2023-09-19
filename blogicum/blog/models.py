a8dd6a01b541cec043b1b6fe954f2501f5031c19
Александр Кузнецов <notjustespo@yandex.ru>
2023-09-19 11:54:26 +0300

    Корректировка файла models

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,13 +1,23 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
+max_value = 256
 
 
-class Location(models.Model):
-    name = models.CharField('Название места', max_length=256)
-    is_published = models.BooleanField('Опубликовано', default=True)
+class BaseModel(models.Model):
+    is_published = models.BooleanField(
+        'Опубликовано',
+        default=True,
+        help_text='Снимите галочку, чтобы скрыть публикацию.')
     created_at = models.DateTimeField('Добавлено', auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Location(BaseModel):
+    name = models.CharField('Название места', max_length=max_value)
 
     class Meta:
         verbose_name = 'местоположение'
@@ -17,19 +27,14 @@ class Location(models.Model):
         return self.name
 
 
-class Category(models.Model):
-    title = models.CharField('Заголовок', max_length=256)
+class Category(BaseModel):
+    title = models.CharField('Заголовок', max_length=max_value)
     description = models.TextField('Описание')
     slug = models.SlugField(
         'Идентификатор',
         unique=True,
-        help_text='Идентификатор страницы для URL; разрешены \
-символы латиницы, цифры, дефис и подчёркивание.')
-    is_published = models.BooleanField(
-        'Опубликовано',
-        default=True,
-        help_text='Снимите галочку, чтобы скрыть публикацию.')
-    created_at = models.DateTimeField('Добавлено', auto_now_add=True)
+        help_text=('Идентификатор страницы для URL; разрешены '
+                   'символы латиницы, цифры, дефис и подчёркивание.'))
 
     class Meta:
         verbose_name = 'категория'
@@ -39,13 +44,13 @@ class Category(models.Model):
         return self.title
 
 
-class Post(models.Model):
-    title = models.CharField('Заголовок', max_length=256)
+class Post(BaseModel):
+    title = models.CharField('Заголовок', max_length=max_value)
     text = models.TextField('Текст')
     pub_date = models.DateTimeField(
         'Дата и время публикации',
-        help_text='Если установить дату и время в будущем — можно делать \
-отложенные публикации.')
+        help_text=('Если установить дату и время в будущем '
+                   '— можно делать отложенные публикации.'))
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -55,20 +60,20 @@ class Post(models.Model):
         Location,
         on_delete=models.SET_NULL,
         null=True,
-        verbose_name='Местоположение'
+        blank=True,
+        verbose_name='Местоположение',
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         null=True,
-        verbose_name='Категория'
+        verbose_name='Категория',
     )
-    is_published = models.BooleanField('Опубликовано', default=True)
-    created_at = models.DateTimeField('Добавлено', auto_now_add=True)
 
     class Meta:
         verbose_name = 'публикация'
         verbose_name_plural = 'Публикации'
+        default_related_name = 'post'
 
     def __str__(self):
         return self.title
